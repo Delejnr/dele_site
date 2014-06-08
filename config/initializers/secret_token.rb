0c5f0082367e11fba32f2b0c0c2547a742410157
delejnr <bj-r1@live.com>
2014-06-08 01:52:41 -0400

    First of a few more commits
    few more commits

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeleSite::Application.config.secret_key_base = '35acafde253afe7e5a9758ab7a80812983dacd0aadf9390dbc4376bceaa0b087b620e7f3166de5f69f556bde10efb95590d5cd62764abffcf2486694aafa6ac3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DeleSite::Application.config.session_store :cookie_store, key: '_dele_site_session'
